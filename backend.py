22f41dccf4971c17a015193b2cebebc11601fb3b
tomaszkoltuniak22@gmail.com <E*E2dL:3endYXT;u~Gk>
2021-06-26 19:56:20 +0200

    Add insert data to db in backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
-from sqlite3 import connect
+from sqlite3 import connect, OperationalError
+from datetime import datetime
 
 
 class AllNotes:
@@ -6,11 +7,38 @@ class AllNotes:
         self.path = 'database.db'
         self.all_notes = []
 
-    def import_data(self):
+    def select_data(self):
+        try:
+            with connect(self.path) as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    'SELECT `priority`, `text`, `category`, `creation_date`, `deadline`, `colour` FROM notes')
+                result = cursor.fetchall()
+                self.all_notes = result
+                return result
+        except OperationalError:
+            with connect(self.path) as connection:
+                cursor = connection.cursor()
+                cursor.execute('CREATE TABLE `notes` ('
+                               '`id` INTEGER PRIMARY KEY AUTOINCREMENT,'
+                               '`priority` TEXT,'
+                               '`text` TEXT,'
+                               '`category` TEXT,'
+                               '`creation_date` DATETIME,'
+                               '`deadline` DATETIME,'
+                               '`colour` TEXT)')
+                return self.select_data()
+
+    def insert_data(self, priority: int, text: str, category: str, deadline: str, colour: str):
+        creation_date = datetime.now().strftime('%Y.%m.%d %H:%M')
         with connect(self.path) as connection:
             cursor = connection.cursor()
-            cursor.execute('SELECT `priority`, `text`, `category`, `creation_date`, `deadline`, `colour` FROM notes')
-            return cursor.fetchall()
+            cursor.execute(
+                'INSERT INTO `notes` (`priority`, `text`, `category`, `creation_date`, `deadline`, `colour`)'
+                'VALUES (?, ?, ?, ?, ?, ?)', (priority, text, category, creation_date, deadline, colour))
+            connection.commit()
+
+# 'YYYY.MM.DD HH:MM'
 # CREATE TABLE `notes` (
 #   `id` int(11) NOT NULL,
 #   `priority` int(11) NOT NULL,
