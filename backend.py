91ed16ab3d294089b66d848f442710c6658d0d05
tomaszkoltuniak22@gmail.com <E*E2dL:3endYXT;u~Gk>
2021-06-26 18:38:24 +0200

    Create sort notes bar

--- backend.py
+++ backend.py
@@ -1,11 +1,21 @@
 from sqlite3 import connect
 
+
+class AllNotes:
+    def __init__(self):
+        self.path = 'database.db'
+        self.all_notes = []
+
+    def import_data(self):
+        with connect(self.path) as connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT `priority`, `text`, `category`, `creation_date`, `deadline`, `colour`')
 # CREATE TABLE `notes` (
 #   `id` int(11) NOT NULL,
-#   `text` text COLLATE utf8_polish_ci NOT NULL,
 #   `priority` int(11) NOT NULL,
+#   `text` text COLLATE utf8_polish_ci NOT NULL,
 #   `category` text COLLATE utf8_polish_ci NOT NULL,
-#   `colour` text COLLATE utf8_polish_ci NOT NULL,
 #   `creation_date` datetime NOT NULL,
-#   `deadline` datetime NOT NULL
+#   `deadline` datetime NOT NULL,
+#   `colour` text COLLATE utf8_polish_ci NOT NULL
 # )
