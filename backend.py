94fb77f24996e45eac3a5286599bacf032ddc90a
Tomasz Kołtuniak <tomaszkoltuniak22@gmail.com>
2021-07-02 18:45:08 +0200

    Add log saving function

--- backend.py
+++ backend.py
@@ -1,5 +1,13 @@
 from sqlite3 import connect, OperationalError
 from datetime import datetime
+from csv import writer
+
+
+def log(module: str, list_of_data: list):
+    data = [datetime.now(), module, list_of_data]
+    with open('log', 'a', encoding='utf-8', newline='') as file:
+        content = writer(file, delimiter=';')
+        content.writerow(data)
 
 
 class AllNotes:
@@ -17,6 +25,7 @@ class AllNotes:
         self.path = 'database.db'
         self.all_notes = []
         self.sort_reverse_status = True
+        log('AllNotes.__init__', [f'DB_path={self.path}'])
 
     def select_data(self):
         try:
@@ -49,6 +58,7 @@ class AllNotes:
                 'INSERT INTO `notes` (`priority`, `text`, `category`, `creation_date`, `deadline`, `colour`)'
                 'VALUES (?, ?, ?, ?, ?, ?)', (priority, text, category, creation_date, deadline, colour))
             connection.commit()
+            log('AllNotes.insert_data', [priority, text, category, creation_date, deadline, colour])
         self.select_data()
 
     def delete_data(self, primary_key: int):
@@ -56,9 +66,11 @@ class AllNotes:
             cursor = connection.cursor()
             cursor.execute('DELETE FROM `notes` WHERE `id` = (?)', (primary_key,))
             connection.commit()
+            log('AllNotes.delete_data', [primary_key])
         self.select_data()
 
     def sort_by(self, sort_criterion: str):
         self.all_notes = sorted(self.all_notes, key=lambda element: element[self.SORT_CRITERION[sort_criterion]],
                                 reverse=self.sort_reverse_status)
+        log('AllNotes.sort_by', [sort_criterion, f'Reverse={self.sort_reverse_status}'])
         self.sort_reverse_status = not self.sort_reverse_status
